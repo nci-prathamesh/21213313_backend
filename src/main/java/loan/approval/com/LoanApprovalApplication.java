707849eb4cb19163de47a1285469bff9d9b3b2f5
nci-prathamesh <124358214+nci-prathamesh@users.noreply.github.com>
2023-04-19 05:27:53 +0100

    aa

--- src/main/java/loan/approval/com/LoanApprovalApplication.java
+++ src/main/java/loan/approval/com/LoanApprovalApplication.java
@@ -2,9 +2,8 @@ package loan.approval.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 public class LoanApprovalApplication {
 
 	public static void main(String[] args) {
